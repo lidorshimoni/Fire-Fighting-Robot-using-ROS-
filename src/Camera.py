2b079d8d68834c7eaf709a94399bb97e4b6af5b6
lidorshimoni <lidor.shim@gmail.com>
2019-12-17 00:34:04 +0200

    Added a new launch file for rviz, added world map at low opacity in rviz. changed camera python file to get camera output using cvbridge.
    Signed-off-by: lidorshimoni <lidor.shim@gmail.com>

--- src/Camera.py
+++ src/Camera.py
@@ -31,36 +31,13 @@ ros_img = rospy.wait_for_message('camera/image_raw/', Image)
 
 while not rospy.is_shutdown():
     img = cv_img
-    img = cv2.resize(img, (300, 300),interpolation = cv2.INTER_AREA)
+    img = cv2.resize(img, (300, 300),interpolation = cv2.INTER_AREA) # for later fire detection
     height = img.shape[0]
     width = img.shape[1]
-   
-    # for i in range(len(rclasses)):
-    #     cls_id = int(rclasses[i])
-    #     if cls_id >= 0:          
-    #         ymin = int(rbboxes[i, 0] * height)
-    #         xmin = int(rbboxes[i, 1] * width)
-    #         ymax = int(rbboxes[i, 2] * height)
-    #         xmax = int(rbboxes[i, 3] * width)
-        
-    #         img = cv2.rectangle(img,(xmin,ymin),(xmax,ymax),colors[cls_id],2)
-    #         font                   = cv2.FONT_HERSHEY_SIMPLEX
-    #         bottomLeftCornerOfText = (xmin,ymin + 20)
-    #         fontScale              = 1
-    #         fontColor              =colors[cls_id]
-    #         lineType               = 2
-            
-    #         img = cv2.putText(img,str(labels.iloc[cls_id][0]), 
-    #                 bottomLeftCornerOfText, 
-    #                 font, 
-    #                 fontScale,
-    #                 fontColor,
-    #                 lineType)
-           
     cv2.imshow('Scanning for fire', img)
+    
 
     
     if cv2.waitKey(1) == 27: 
-        cam.release()
         cv2.destroyAllWindows()
         break  # esc to quit
