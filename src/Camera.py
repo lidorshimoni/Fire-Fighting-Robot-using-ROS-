2c2324947a79684a0b3871302e4531ad84b05ccc
lidorshimoni <lidor.shim@gmail.com>
2019-12-19 20:15:05 +0200

    Added robot_vel to image view node. need to incease movement speed.
    Signed-off-by: lidorshimoni <lidor.shim@gmail.com>

--- src/Camera.py
+++ src/Camera.py
@@ -7,6 +7,7 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
 
 #matplotlib inline
 import matplotlib.pyplot as plt
@@ -14,6 +15,7 @@ import matplotlib.image as mpimg
 
 
 cv_img = 0
+robot_vel = 0
 
 def img_callback (ros_img):
     # print ('got an image')
@@ -22,6 +24,10 @@ def img_callback (ros_img):
         cv_img = bridge.imgmsg_to_cv2(ros_img,"rgb8")
     except CvBridgeError as e:
         print (e)
+    
+def robot_vel_callback(twist):
+    global robot_vel
+    robot_vel = twist
 
 bridge = CvBridge()
 
@@ -29,13 +35,34 @@ rospy.init_node('detect', anonymous = True)
 img_sub = rospy.Subscriber('camera/image_raw/', Image, img_callback)
 ros_img = rospy.wait_for_message('camera/image_raw/', Image)
 
+img_sub = rospy.Subscriber('/cmd_vel', Twist, robot_vel_callback)
+ros_img = rospy.wait_for_message('/cmd_vel', Twist)
+
 while not rospy.is_shutdown():
     img = cv_img
     img = cv2.resize(img, (300, 300),interpolation = cv2.INTER_AREA) # for later fire detection
     height = img.shape[0]
     width = img.shape[1]
-    cv2.imshow('Scanning for fire', img)
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale              = 0.5
+    fontColor              = (255,0,0)
+    lineType               = 2
 
+    cv2.putText(img,'linear: ' + str(round(robot_vel.linear.x, 2)), 
+    (10,20), 
+    font, 
+    fontScale,
+    fontColor,
+    lineType)
+
+    cv2.putText(img,"angular: " + str(round(robot_vel.angular.z, 2)), 
+    (10,35), 
+    font, 
+    fontScale,
+    fontColor,
+    lineType)
+
+    cv2.imshow('Scanning for fire', img)
 
     
     if cv2.waitKey(1) == 27: 
